f730b7af25c7d3d7ddb313d3716e5a17a152cc21
cindy zhao <xinyuezhao96@gmail.com>
2020-07-28 16:56:27 -0700

    update cidr module

--- plugins/modules/aci_cloud_cidr.py
+++ plugins/modules/aci_cloud_cidr.py
@@ -1,114 +1,122 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# Copyright: (c) 2020, nkatarmal-crest <nirav.katarmal@crestdatasys.com>
+# Copyright: (c) 2020, Cindy Zhao <cizhao@cisco.com>
+# GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 DOCUMENTATION = r'''
 ---
-module: aci_cloud_cidr 
-short_description: Manage Cloud CIDR Pool (cloud:Cidr)
+module: aci_cloud_cidr
+short_description: Manage Cloud CIDR on Cisco Cloud ACI.
 description:
 - Mo doc not defined in techpub!!!
 notes:
 - More information about the internal APIC class B(cloud:Cidr) from
   L(the APIC Management Information Model reference,https://developer.cisco.com/docs/apic-mim-ref/).
 author:
-- nirav (@nirav.katarmal)
-options: 
-  addr:
+- Nirav (@nirav)
+- Cindy Zhao (@cizhao)
+options:
+  address:
     description:
-    - peer address 
-  descr:
+    - CIDR ip and its submask.
+    type: str
+  description:
     description:
-    - configuration item description. 
-  nameAlias:
+    - Description of the Cloud CIDR.
+    type: str
+  name_alias:
     description:
-    - Mo doc not defined in techpub!!! 
+    - An alias for the name of the current object. This relates to the nameAlias field in ACI and is used to rename object without changing the DN.
+    type: str
   primary:
     description:
-    - Mo doc not defined in techpub!!! 
-    choices: [ no, yes ] 
+    - Whether this is the primary CIDR
+    choices: [ 'yes', 'no' ]
+    default: 'no'
+    type: str
   tenant:
     description:
-    - tenant name 
+    - The name of the Tenant.
+    type: str
   cloud_context_profile:
     description:
-    - object name 
-  state: 
+    - The name of the Cloud Context Profile.
+    type: str
+  state:
     description:
     - Use C(present) or C(absent) for adding or removing.
     - Use C(query) for listing an object or multiple objects.
     choices: [ absent, present, query ]
-    default: present 
+    default: present
+    type: str
 
-extends_documentation_fragment: aci
+extends_documentation_fragment:
+- cisco.aci.aci
 '''
+
 
 from ansible_collections.cisco.aci.plugins.module_utils.aci import ACIModule, aci_argument_spec
 from ansible.module_utils.basic import AnsibleModule
 
+
 def main():
     argument_spec = aci_argument_spec()
-    argument_spec.update({ 
-        'addr': dict(type='str',),
-        'annotation': dict(type='str',),
-        'descr': dict(type='str',),
-        'nameAlias': dict(type='str',),
-        'ownerKey': dict(type='str',),
-        'ownerTag': dict(type='str',),
-        'primary': dict(type='str', choices=['no', 'yes'], ),
-        'tenant': dict(type='str',),
-        'cloud_context_profile': dict(type='str',),
-        'state': dict(type='str', default='present', choices=['absent', 'present', 'query']),
-
-    })
+    argument_spec.update(
+        address=dict(type='str',),
+        description=dict(type='str',),
+        name_alias=dict(type='str',),
+        primary=dict(type='str', default='no', choices=['no', 'yes']),
+        tenant=dict(type='str'),
+        cloud_context_profile=dict(type='str'),
+        state=dict(type='str', default='present', choices=['absent', 'present', 'query']),
+    )
 
     module = AnsibleModule(
         argument_spec=argument_spec,
         supports_check_mode=True,
-        required_if=[ 
-            ['state', 'absent', ['addr', 'tenant', 'cloud_context_profile', ]], 
-            ['state', 'present', ['addr', 'tenant', 'cloud_context_profile', ]],
+        required_if=[
+            ['state', 'absent', ['address', 'tenant', 'cloud_context_profile', ]],
+            ['state', 'present', ['address', 'tenant', 'cloud_context_profile', ]],
         ],
     )
-    
-    addr = module.params['addr']
-    annotation = module.params['annotation']
-    descr = module.params['descr']
-    nameAlias = module.params['nameAlias']
-    ownerKey = module.params['ownerKey']
-    ownerTag = module.params['ownerTag']
-    primary = module.params['primary']
-    tenant = module.params['tenant']
-    cloud_context_profile = module.params['cloud_context_profile']
-    state = module.params['state']
-    child_configs=[]
-    
+
+    address = module.params.get('address')
+    description = module.params.get('description')
+    name_alias = module.params.get('name_alias')
+    primary = module.params.get('primary')
+    tenant = module.params.get('tenant')
+    cloud_context_profile = module.params.get('cloud_context_profile')
+    state = module.params.get('state')
+    child_configs = []
 
     aci = ACIModule(module)
     aci.construct_url(
-        root_class={
-            'aci_class': 'fvTenant',
-            'aci_rn': 'tn-{}'.format(tenant),
-            'target_filter': 'eq(fvTenant.name, "{}")'.format(tenant),
-            'module_object': tenant
-        }, 
-        subclass_1={
-            'aci_class': 'cloudCtxProfile',
-            'aci_rn': 'ctxprofile-{}'.format(cloud_context_profile),
-            'target_filter': 'eq(cloudCtxProfile.name, "{}")'.format(cloud_context_profile),
-            'module_object': cloud_context_profile
-        }, 
-        subclass_2={
-            'aci_class': 'cloudCidr',
-            'aci_rn': 'cidr-[{}]'.format(addr),
-            'target_filter': 'eq(cloudCidr.addr, "{}")'.format(addr),
-            'module_object': addr
-        }, 
-        
+        root_class=dict(
+            aci_class='fvTenant',
+            aci_rn='tn-{0}'.format(tenant),
+            target_filter='eq(fvTenant.name, "{0}")'.format(tenant),
+            module_object=tenant
+        ),
+        subclass_1=dict(
+            aci_class='cloudCtxProfile',
+            aci_rn='ctxprofile-{0}'.format(cloud_context_profile),
+            target_filter='eq(cloudCtxProfile.name, "{0}")'.format(cloud_context_profile),
+            module_object=cloud_context_profile
+        ),
+        subclass_2=dict(
+            aci_class='cloudCidr',
+            aci_rn='cidr-[{0}]'.format(address),
+            target_filter='eq(cloudCidr.addr, "{0}")'.format(address),
+            module_object=address
+        ),
+
         child_classes=[]
-        
+
     )
 
     aci.get_existing()
@@ -116,17 +124,13 @@ def main():
     if state == 'present':
         aci.payload(
             aci_class='cloudCidr',
-            class_config={ 
-                'addr': addr,
-                'annotation': annotation,
-                'descr': descr,
-                'nameAlias': nameAlias,
-                'ownerKey': ownerKey,
-                'ownerTag': ownerTag,
-                'primary': primary,
-            },
+            class_config=dict(
+                addr=address,
+                descr=description,
+                nameAlias=name_alias,
+                primary=primary,
+            ),
             child_configs=child_configs
-           
         )
 
         aci.get_diff(aci_class='cloudCidr')
@@ -137,6 +141,7 @@ def main():
         aci.delete_config()
 
     aci.exit_json()
+
 
 if __name__ == "__main__":
     main()
