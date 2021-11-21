da5e88c7853e921033716c4da3e7915af1e7544e
Shreyas <ssrish@cisco.com>
2021-11-20 20:03:30 -0800

    Put back inventory

--- plugins/doc_fragments/annotation.py
+++ plugins/doc_fragments/annotation.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2017, Dag Wieers (@dagwieers) <dag@wieers.com>
-# Copyright: (c) 2017, Swetha Chunduri (@schunduri)
 # GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import (absolute_import, division, print_function)
