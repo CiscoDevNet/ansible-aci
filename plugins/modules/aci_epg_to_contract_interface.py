b9112c82cebb1fe9e2fad812699bee764ff1d402
Sabari Jaganathan <sajagana@cisco.com>
2021-12-16 19:48:45 +0530

    Removed default value on priority parameter
    and changed contract to contract_interface on module and test files

--- plugins/modules/aci_epg_to_contract_interface.py
+++ plugins/modules/aci_epg_to_contract_interface.py
@@ -7,10 +7,10 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-ANSIBLE_METADATA = {'metadata_version': '1.1', 'status': ['preview'], 'supported_by': 'certified'}
+ANSIBLE_METADATA = {"metadata_version": "1.1", "status": ["preview"], "supported_by": "certified"}
 
 
-DOCUMENTATION = r'''
+DOCUMENTATION = r"""
 ---
 module: aci_epg_to_contract_interface
 short_description: Bind EPGs to Consumed Contracts Interface (fv:RsConsIf).
@@ -18,8 +18,8 @@ short_description: Bind EPGs to Consumed Contracts Interface (fv:RsConsIf).
 description:
 - Bind EPGs to Consumed Contracts Interface on Cisco ACI fabrics.
 notes:
-- The C(tenant), C(app_profile), C(EPG), and C(Contract) used must exist before using this module in your playbook.
-  The M(cisco.aci.aci_tenant), M(cisco.aci.aci_ap), M(cisco.aci.aci_epg), and M(cisco.aci.aci_contract) modules can be used for this.
+- The C(tenant), C(app_profile), C(EPG), and C(Contract Interface) used must exist before using this module in your playbook.
+  The M(cisco.aci.aci_tenant), M(cisco.aci.aci_ap), M(cisco.aci.aci_epg), and M(cisco.aci.aci_contract_export) modules can be used for this.
 options:
   tenant:
     description:
@@ -36,9 +36,9 @@ options:
     - The name of the end point group.
     type: str
     aliases: [ epg_name ]
-  contract:
+  contract_interface:
     description:
-    - Name of the Contract interface, which is exported to the tenant with the scope of 'Global'.
+    - Name of the Contract interface, which is the contract with a "Global" scope which is exported to the tenant.
     type: str
     aliases: [ contract_interface_name ]
   priority:
@@ -47,7 +47,6 @@ options:
     - The default value of QoS is C(unspecified) during the creation.
     type: str
     choices: [ level1, level2, level3, level4, level5, level6, unspecified ]
-    default: unspecified
   state:
     description:
     - Use C(present) or C(absent) for adding or removing.
@@ -62,14 +61,14 @@ extends_documentation_fragment:
 seealso:
 - module: cisco.aci.aci_ap
 - module: cisco.aci.aci_epg
-- module: cisco.aci.aci_contract
+- module: cisco.aci.aci_contract_export
 - name: APIC Management Information Model reference
   description: More information about the internal APIC classes B(fv:RsCons) and B(fv:RsProv).
   link: https://developer.cisco.com/docs/apic-mim-ref/
 author:
 - Sabari Jaganathan (@sajagana)
-'''
-EXAMPLES = r'''
+"""
+EXAMPLES = r"""
 - name: Add a new consumed contract interface to EPG
   cisco.aci.aci_epg_to_contract_interface:
     host: apic
@@ -78,7 +77,7 @@ EXAMPLES = r'''
     tenant: anstest
     ap: anstest
     epg: anstest
-    contract: anstest_http
+    contract_interface: anstest_http
     priority: level3
     state: present
   delegate_to: localhost
@@ -91,7 +90,7 @@ EXAMPLES = r'''
     tenant: anstest
     ap: anstest
     epg: anstest
-    contract: anstest_http
+    contract_interface: anstest_http
     state: absent
   delegate_to: localhost
 
@@ -103,7 +102,7 @@ EXAMPLES = r'''
     tenant: anstest
     ap: anstest
     epg: anstest
-    contract: anstest_http
+    contract_interface: anstest_http
     state: query
   delegate_to: localhost
   register: query_result
@@ -116,9 +115,9 @@ EXAMPLES = r'''
     state: query
   delegate_to: localhost
   register: query_result
-'''
+"""
 
-RETURN = r'''
+RETURN = r"""
 current:
   description: The existing configuration from the APIC after the module has finished
   returned: success
@@ -221,101 +220,83 @@ url:
   returned: failure or debug
   type: str
   sample: https://10.11.12.13/api/mo/uni/tn-production.json
-'''
+"""
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.cisco.aci.plugins.module_utils.aci import ACIModule, aci_argument_spec
-
-ACI_CLASS_MAPPING = dict(
-    consumer={
-        'class': 'fvRsCons',
-        'rn': 'rscons-',
-    },
-    provider={
-        'class': 'fvRsProv',
-        'rn': 'rsprov-',
-    },
-)
-
-PROVIDER_MATCH_MAPPING = dict(
-    all='All',
-    at_least_one='AtleastOne',
-    at_most_one='AtmostOne',
-    none='None',
-)
 
 
 def main():
     argument_spec = aci_argument_spec()
     argument_spec.update(
-        tenant=dict(type='str', aliases=['tenant_name']),  # Not required for querying all objects
-        ap=dict(type='str', aliases=['app_profile', 'app_profile_name']),  # Not required for querying all objects
-        epg=dict(type='str', aliases=['epg_name']),  # Not required for querying all objects
-        contract=dict(type='str', aliases=['contract_interface_name']),  # Not required for querying all objects
-        priority=dict(type='str', default='unspecified', choices=['level1', 'level2', 'level3', 'level4', 'level5', 'level6', 'unspecified']),
-        state=dict(type='str', default='present', choices=['absent', 'present', 'query']),
+        tenant=dict(type="str", aliases=["tenant_name"]),  # Not required for querying all objects
+        ap=dict(type="str", aliases=["app_profile", "app_profile_name"]),  # Not required for querying all objects
+        epg=dict(type="str", aliases=["epg_name"]),  # Not required for querying all objects
+        contract_interface=dict(type="str", aliases=["contract_interface_name"]),  # Not required for querying all objects
+        priority=dict(type="str", choices=["level1", "level2", "level3", "level4", "level5", "level6", "unspecified"]),
+        state=dict(type="str", default="present", choices=["absent", "present", "query"]),
     )
 
     module = AnsibleModule(
         argument_spec=argument_spec,
         supports_check_mode=True,
         required_if=[
-            ['state', 'absent', ['tenant', 'ap', 'epg', 'contract', 'priority']],
-            ['state', 'present', ['tenant', 'ap', 'epg', 'contract', 'priority']],
+            ["state", "absent", ["tenant", "ap", "epg", "contract_interface"]],
+            ["state", "present", ["tenant", "ap", "epg", "contract_interface"]],
         ],
     )
 
-    tenant = module.params.get('tenant')
-    ap = module.params.get('ap')
-    epg = module.params.get('epg')
-    contract = module.params.get('contract')
-    priority = module.params.get('priority')
-    state = module.params.get('state')
+    tenant = module.params.get("tenant")
+    ap = module.params.get("ap")
+    epg = module.params.get("epg")
+    contract_interface = module.params.get("contract_interface")
+    priority = module.params.get("priority")
+    state = module.params.get("state")
 
     aci = ACIModule(module)
     aci.construct_url(
         root_class=dict(
-            aci_class='fvTenant',
-            aci_rn='tn-{0}'.format(tenant),
+            aci_class="fvTenant",
+            aci_rn="tn-{0}".format(tenant),
             module_object=tenant,
-            target_filter={'name': tenant},
+            target_filter={"name": tenant},
         ),
         subclass_1=dict(
-            aci_class='fvAp',
-            aci_rn='ap-{0}'.format(ap),
+            aci_class="fvAp",
+            aci_rn="ap-{0}".format(ap),
             module_object=ap,
-            target_filter={'name': ap},
+            target_filter={"name": ap},
         ),
         subclass_2=dict(
-            aci_class='fvAEPg',
-            aci_rn='epg-{0}'.format(epg),
+            aci_class="fvAEPg",
+            aci_rn="epg-{0}".format(epg),
             module_object=epg,
-            target_filter={'name': epg},
+            target_filter={"name": epg},
         ),
         subclass_3=dict(
-            aci_class='fvRsConsIf',
-            aci_rn='rsconsIf-{0}'.format(contract),
-            module_object=contract,
-            target_filter={'name': contract},
+            aci_class="fvRsConsIf",
+            aci_rn="rsconsIf-{0}".format(contract_interface),
+            module_object=contract_interface,
+            target_filter={"name": contract_interface},
         ),
     )
 
     aci.get_existing()
 
-    if state == 'present':
+    if state == "present":
         aci.payload(
-            aci_class='fvRsConsIf',
+            aci_class="fvRsConsIf",
             class_config=dict(
                 prio=priority,
-                tnVzCPIfName=contract,
+                tnVzCPIfName=contract_interface,
             ),
         )
 
-        aci.get_diff(aci_class='fvRsConsIf')
+        aci.get_diff(aci_class="fvRsConsIf")
 
         aci.post_config()
 
-    elif state == 'absent':
+    elif state == "absent":
         aci.delete_config()
 
     aci.exit_json()
