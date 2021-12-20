1c8fa34e09edc4702413a429971ee0d01c91162a
Lionel Hercot <lhercot@cisco.com>
2021-12-20 03:42:15 +0100

    Refactor test cases for aci_tag and run black against aci_tag module code

--- plugins/modules/aci_tag.py
+++ plugins/modules/aci_tag.py
@@ -4,13 +4,12 @@
 # GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
+
 __metaclass__ = type
 
-ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['preview'],
-                    'supported_by': 'certified'}
+ANSIBLE_METADATA = {"metadata_version": "1.1", "status": ["preview"], "supported_by": "certified"}
 
-DOCUMENTATION = r'''
+DOCUMENTATION = r"""
 ---
 module: aci_tag
 short_description: Tagging of ACI objects
@@ -34,6 +33,7 @@ options:
     - Type of tag object.
     type: str
     choices: [ annotation, instance, tag ]
+    required: yes
   state:
     description:
     - Use C(present) or C(absent) for adding or removing.
@@ -55,9 +55,9 @@ seealso:
   link: https://www.cisco.com/c/en/us/support/cloud-systems-management/application-policy-infrastructure-controller-apic/tsd-products-support-series-home.html
 author:
 - Akini Ross (@akinross)
-'''
+"""
 
-EXAMPLES = r'''
+EXAMPLES = r"""
 - name: Add a new annotation tag
   cisco.aci.aci_tag:
     host: apic
@@ -97,9 +97,9 @@ EXAMPLES = r'''
     tag_type: annotation
     state: query
   delegate_to: localhost
-'''
+"""
 
-RETURN = r'''
+RETURN = r"""
 current:
   description: The existing configuration from the APIC after the module has finished
   returned: success
@@ -202,7 +202,7 @@ url:
   returned: failure or debug
   type: str
   sample: https://10.11.12.13/api/mo/uni/tn-production.json
-'''
+"""
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.cisco.aci.plugins.module_utils.aci import ACIModule, aci_argument_spec
@@ -211,59 +211,56 @@ from ansible_collections.cisco.aci.plugins.module_utils.aci import ACIModule, ac
 def main():
     argument_spec = aci_argument_spec()
     argument_spec.update(
-        dn=dict(type='str'),
-        tag_key=dict(type='str'),
-        tag_value=dict(type='str', default=''),
-        tag_type=dict(type='str', choices=['annotation', 'instance', 'tag']),
-        state=dict(type='str', default='present', choices=['absent', 'present', 'query']),
+        dn=dict(type="str"),
+        tag_key=dict(type="str"),
+        tag_value=dict(type="str", default=""),
+        tag_type=dict(type="str", choices=["annotation", "instance", "tag"], required=True),
+        state=dict(type="str", default="present", choices=["absent", "present", "query"]),
     )
 
     module = AnsibleModule(
         argument_spec=argument_spec,
         supports_check_mode=True,
         required_if=[
-            ['state', 'query', ['tag_type']],
-            ['state', 'absent', ['tag_type', 'tag_key']],
-            ['state', 'present', ['tag_type', 'tag_key']],
+            ["state", "absent", ["dn", "tag_key"]],
+            ["state", "present", ["dn", "tag_key"]],
         ],
-        required_together=[('tag_key', 'dn')]
     )
 
     aci = ACIModule(module)
 
-    dn = module.params.get('dn') if module.params.get('dn') is None else module.params.get('dn').lstrip("uni/")
-    tag_key = module.params.get('tag_key')
-    tag_value = module.params.get('tag_value')
-    tag_type = module.params.get('tag_type')
-    state = module.params.get('state')
+    dn = module.params.get('dn')
+    tag_key = module.params.get("tag_key")
+    tag_value = module.params.get("tag_value")
+    tag_type = module.params.get("tag_type")
+    state = module.params.get("state")
 
-    aci_type = dict(annotation=dict(dn='{0}/annotationKey-{1}'.format(dn, tag_key), name="tagAnnotation",
-                                    config=dict(value=tag_value)),
-                    instance=dict(dn='{0}/tag-{1}'.format(dn, tag_key), name='tagInst', config=dict()),
-                    tag=dict(dn='{0}/tagKey-{1}'.format(dn, tag_key), name="tagTag", config=dict(value=tag_value)))
+    if module.params.get('dn') is not None:
+        dn = dn.lstrip("uni/")
+
+    aci_type = dict(
+        annotation=dict(dn="{0}/annotationKey-{1}".format(dn, tag_key), name="tagAnnotation", config=dict(value=tag_value)),
+        instance=dict(dn="{0}/tag-{1}".format(dn, tag_key), name="tagInst", config=dict()),
+        tag=dict(dn="{0}/tagKey-{1}".format(dn, tag_key), name="tagTag", config=dict(value=tag_value)),
+    )
 
     aci.construct_url(
         root_class=dict(
-            aci_class=aci_type[tag_type]['name'],
-            aci_rn=aci_type[tag_type]['dn'],
+            aci_class=aci_type[tag_type]["name"],
+            aci_rn=aci_type[tag_type]["dn"],
             module_object=tag_key,
-            target_filter={'name': tag_key},
+            target_filter={"name": tag_key},
         )
     )
 
     aci.get_existing()
 
-    if state == 'present':
-
-        aci.payload(
-            aci_class=aci_type[tag_type]['name'],
-            class_config=aci_type[tag_type]['config']
-        )
-        aci.get_diff(aci_class=aci_type[tag_type]['name'])
+    if state == "present":
+        aci.payload(aci_class=aci_type[tag_type]["name"], class_config=aci_type[tag_type]["config"])
+        aci.get_diff(aci_class=aci_type[tag_type]["name"])
         aci.post_config()
 
-    elif state == 'absent':
-
+    elif state == "absent":
         aci.delete_config()
 
     aci.exit_json()
