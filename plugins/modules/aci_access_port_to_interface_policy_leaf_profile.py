5dabab1019865e31c1d2585447dff173143ea505
Sabari Jaganathan <sajagana@cisco.com>
2022-03-07 18:02:40 +0530

    Added string variable for same dn pattern

--- plugins/modules/aci_access_port_to_interface_policy_leaf_profile.py
+++ plugins/modules/aci_access_port_to_interface_policy_leaf_profile.py
@@ -306,14 +306,16 @@ url:
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.cisco.aci.plugins.module_utils.aci import ACIModule, aci_argument_spec, aci_annotation_spec, aci_owner_spec
 
+port_channels_dn = "uni/infra/funcprof/accbundle-{0}"
+
 INTERFACE_TYPE_MAPPING = dict(
     breakout="uni/infra/funcprof/brkoutportgrp-{0}",
     fex="uni/infra/funcprof/accportgrp-{0}",
-    port_channel="uni/infra/funcprof/accbundle-{0}",
+    port_channel=port_channels_dn,
     switch_port="uni/infra/funcprof/accportgrp-{0}",
-    vpc="uni/infra/funcprof/accbundle-{0}",
-    fex_port_channel="uni/infra/funcprof/accbundle-{0}",
-    fex_vpc="uni/infra/funcprof/accbundle-{0}",
+    vpc=port_channels_dn,
+    fex_port_channel=port_channels_dn,
+    fex_vpc=port_channels_dn,
 )
 
 
