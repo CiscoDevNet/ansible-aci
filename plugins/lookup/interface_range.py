8e1cf4b6e94c5c8d0894642e73c392af44cb7afc
akinross <akinross@cisco.com>
2022-06-22 09:15:07 +0200

    [minor_change] flexible slot input and single interface in range

--- plugins/lookup/interface_range.py
+++ plugins/lookup/interface_range.py
@@ -26,7 +26,7 @@ EXAMPLES = """
 - name: "loop through range of interfaces"
   ansible.builtin.debug:
     msg: "{{ item }}"
-  with_items: "{{ query('cisco.aci.interface_range', '1/1-4,1/20-25,1/31-32,2/10-15,2/11-12,2/13-13', '1/5-9,4/8-10', '5/0-2') }}"
+  with_items: "{{ query('cisco.aci.interface_range', '1/1-4,1/20-25', '1/5', '1/2/3/8-10', '5/0-2') }}"
 """
 
 RETURN = """
@@ -46,21 +46,24 @@ class LookupModule(LookupBase):
 
     def run(self, terms, **kwargs):
 
-        int_range = ','.join(terms).replace(" ", "")
-        # Regex could be made more specific to match max amount of slots/ports supported in nexus devices
-        val_regex = "([0-9]/[0-9]{1,2}-[0-9]{1,2})|(^([0-9]/[0-9]{1,2}-[0-9]{1,2},)+([0-9]/[0-9]{1,2}-[0-9]{1,2})$)"
         interfaces = []
+        errors = []
 
-        if re.fullmatch(val_regex, int_range):
-
-            for r in [(r.split("/")[0], int(r.split("/")[1].split("-")[0]), int(r.split("/")[1].split("-")[1])) for r in int_range.split(",")]:
-                if r[2] >= r[1]:
-                    for x in range(r[1], r[2] + 1):
-                        interfaces.append(f"{r[0]}/{x}")
+        for interface_range in ','.join(terms).replace(" ", "").split(","):
+            if re.fullmatch(r"((\d+/)+\d+-\d+$)", interface_range):
+                slots = interface_range.rsplit("/", 1)[0]
+                range_start, range_stop = interface_range.rsplit("/", 1)[1].split("-")
+                if int(range_stop) > int(range_start):
+                    for x in range(int(range_start), int(range_stop) + 1):
+                        interfaces.append("{0}/{1}".format(slots, x))
                 else:
-                    raise AnsibleError("INVALID RANGE INPUT: End '{0}/{1}-{2}' is smaller than begin of interface range.".format(r[0], r[1], r[2]))
-        else:
-            raise AnsibleError("INVALID RANGE INPUT: Range not matching pattern '1/20-21' or '1/20-21,1/24-27' for interface ranges.")
+                    errors.append(interface_range)
+            elif re.fullmatch(r"((\d+/)+\d+$)", interface_range):
+                interfaces.append(interface_range)
+            else:
+                errors.append(interface_range)
+        if errors:
+            raise AnsibleError("Invalid range inputs, {0}".format(errors))
 
         # Sorted functionality for visual aid only, will result in 1/25, 1/3, 1/31
         # If full sort is needed leverage natsort package (https://github.com/SethMMorton/natsort)
