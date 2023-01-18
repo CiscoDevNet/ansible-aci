a19ac7e07a495aca7f93d0e8a79262c91dd5a05b
Sabari Jaganathan <93724860+sajagana@users.noreply.github.com>
2023-01-18 16:41:26 +0530

    [minor_change] Add Node Profile BGP Peer and Route Control Profile functionalities to aci_l3out_bgp_peer module (#340)

--- plugins/modules/aci_l3out_bgp_peer.py
+++ plugins/modules/aci_l3out_bgp_peer.py
@@ -7,7 +7,11 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-ANSIBLE_METADATA = {"metadata_version": "1.1", "status": ["preview"], "supported_by": "community"}
+ANSIBLE_METADATA = {
+    "metadata_version": "1.1",
+    "status": ["preview"],
+    "supported_by": "community",
+}
 
 DOCUMENTATION = r"""
 ---
@@ -21,48 +25,40 @@ options:
     - Name of an existing tenant.
     type: str
     aliases: [ tenant_name ]
-    required: yes
   l3out:
     description:
     - Name of an existing L3Out.
     type: str
     aliases: [ l3out_name ]
-    required: yes
   node_profile:
     description:
     - Name of the node profile.
     type: str
     aliases: [ node_profile_name, logical_node ]
-    required: yes
   interface_profile:
     description:
     - Name of the interface profile.
     type: str
     aliases: [ interface_profile_name, logical_interface ]
-    required: yes
   pod_id:
     description:
     - Pod to build the interface on.
     type: str
-    required: yes
   node_id:
     description:
     - Node to build the interface on for Port-channels and single ports.
     - Hyphen separated pair of nodes (e.g. "201-202") for vPCs.
     type: str
-    required: yes
   path_ep:
     description:
     - Path to interface
     - Interface Port Group name for Port-channels and vPCs
     - Port number for single ports (e.g. "eth1/12")
     type: str
-    required: yes
   peer_ip:
     description:
     - IP address of the BGP peer.
     type: str
-    required: yes
   remote_asn:
     description:
     - Autonomous System Number of the BGP peer.
@@ -109,6 +105,31 @@ options:
     - Number of allowed self AS.
     - Only used if C(allow-self-as) is enabled under C(bgp_controls).
     type: int
+  route_control_profiles:
+    description:
+    - List of dictionaries objects, which is used to bind the BGP Peer Connectivity Profile to Route Control Profile.
+    type: list
+    elements: dict
+    suboptions:
+      tenant:
+        description:
+        - Name of the tenant.
+        type: str
+        required: yes
+      profile:
+        description:
+        - Name of the Route Control Profile.
+        type: str
+        required: yes
+      l3out:
+        description:
+        - Name of the L3 Out.
+        type: str
+      direction:
+        description:
+        - Name of the Route Control Profile direction.
+        type: str
+        required: yes
   state:
     description:
     - Use C(present) or C(absent) for adding or removing.
@@ -151,7 +172,15 @@ EXAMPLES = r"""
       - send-ext-com
     peer_controls:
       - bfd
-    state: present
+    route_control_profiles:
+      - tenant: "ansible_tenant"
+        profile: "anstest_import"
+        direction: "import"
+      - tenant: "ansible_tenant"
+        profile: "anstest_export"
+        direction: "export"
+        l3out: "anstest_l3out"
+        state: present
   delegate_to: localhost
 
 - name: Add a new BGP peer on a vPC
@@ -206,6 +235,25 @@ EXAMPLES = r"""
     state: absent
   delegate_to: localhost
 
+- name: Add BGP Peer to the Node Profile level
+  cisco.aci.aci_l3out_bgp_peer:
+    host: apic
+    username: admin
+    password: SomeSecretPassword
+    tenant: ansible_tenant
+    l3out: ansible_l3out
+    node_profile: ansible_node_profile
+    peer_ip: 192.168.50.3
+    route_control_profiles:
+      - tenant: "ansible_tenant"
+        profile: "anstest_import"
+        direction: "import"
+      - tenant: "ansible_tenant"
+        profile: "anstest_export"
+        direction: "export"
+        l3out: "anstest_l3out"
+    state: present
+
 - name: Query a BGP peer
   cisco.aci.aci_l3out_bgp_peer:
     host: apic
@@ -222,6 +270,15 @@ EXAMPLES = r"""
     state: query
   delegate_to: localhost
   register: query_result
+
+- name: Query all BGP peer
+  cisco.aci.aci_l3out_bgp_peer:
+    host: apic
+    username: admin
+    password: SomeSecretPassword
+    state: query
+  delegate_to: localhost
+  register: query_all
 
 """
 
@@ -332,34 +389,67 @@ url:
 
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.cisco.aci.plugins.module_utils.aci import ACIModule, aci_argument_spec, aci_annotation_spec
+from ansible_collections.cisco.aci.plugins.module_utils.aci import (
+    ACIModule,
+    aci_argument_spec,
+    aci_annotation_spec,
+    route_control_profile_spec,
+)
 
 
 def main():
     argument_spec = aci_argument_spec()
     argument_spec.update(aci_annotation_spec())
     argument_spec.update(
-        tenant=dict(type="str", aliases=["tenant_name"], required=True),
-        l3out=dict(type="str", aliases=["l3out_name"], required=True),
-        node_profile=dict(type="str", aliases=["node_profile_name", "logical_node"], required=True),
-        interface_profile=dict(type="str", aliases=["interface_profile_name", "logical_interface"], required=True),
+        tenant=dict(type="str", aliases=["tenant_name"]),
+        l3out=dict(type="str", aliases=["l3out_name"]),
+        node_profile=dict(type="str", aliases=["node_profile_name", "logical_node"]),
+        interface_profile=dict(type="str", aliases=["interface_profile_name", "logical_interface"]),
         state=dict(type="str", default="present", choices=["absent", "present", "query"]),
-        pod_id=dict(type="str", required=True),
-        node_id=dict(type="str", required=True),
-        path_ep=dict(type="str", required=True),
-        peer_ip=dict(type="str", required=True),
+        pod_id=dict(type="str"),
+        node_id=dict(type="str"),
+        path_ep=dict(type="str"),
+        peer_ip=dict(type="str"),
         remote_asn=dict(type="int"),
-        bgp_controls=dict(type="list", elements="str", choices=["send-com", "send-ext-com", "allow-self-as", "as-override", "dis-peer-as-check", "nh-self"]),
+        bgp_controls=dict(
+            type="list",
+            elements="str",
+            choices=[
+                "send-com",
+                "send-ext-com",
+                "allow-self-as",
+                "as-override",
+                "dis-peer-as-check",
+                "nh-self",
+            ],
+        ),
         peer_controls=dict(type="list", elements="str", choices=["bfd", "dis-conn-check"]),
         address_type_controls=dict(type="list", elements="str", choices=["af-ucast", "af-mcast"]),
-        private_asn_controls=dict(type="list", elements="str", choices=["remove-exclusive", "remove-all", "replace-as"]),
+        private_asn_controls=dict(
+            type="list",
+            elements="str",
+            choices=["remove-exclusive", "remove-all", "replace-as"],
+        ),
         ttl=dict(type="int"),
         weight=dict(type="int"),
         admin_state=dict(type="str", choices=["enabled", "disabled"]),
         allow_self_as_count=dict(type="int"),
+        route_control_profiles=dict(
+            type="list",
+            elements="dict",
+            options=route_control_profile_spec(),
+        ),
     )
 
-    module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=True)
+    module = AnsibleModule(
+        argument_spec=argument_spec,
+        supports_check_mode=True,
+        required_if=[
+            ["state", "absent", ["tenant", "l3out", "node_profile", "peer_ip"]],
+            ["state", "present", ["tenant", "l3out", "node_profile", "peer_ip"]],
+        ],
+        required_together=[["interface_profile", "pod_id", "node_id", "path_ep"]],
+    )
 
     tenant = module.params.get("tenant")
     l3out = module.params.get("l3out")
@@ -373,20 +463,91 @@ def main():
     remote_asn = module.params.get("remote_asn")
     bgp_controls = module.params.get("bgp_controls")
     peer_controls = module.params.get("peer_controls")
-    address_type_controls = module.params.get("address_type_controls")
+    address_type_controls = sorted(module.params.get("address_type_controls") or [])
     private_asn_controls = module.params.get("private_asn_controls")
     ttl = module.params.get("ttl")
     weight = module.params.get("weight")
     admin_state = module.params.get("admin_state")
     allow_self_as_count = module.params.get("allow_self_as_count")
+    route_control_profiles = module.params.get("route_control_profiles")
 
     aci = ACIModule(module)
-    if "-" in node_id:
-        path_type = "protpaths"
-    else:
-        path_type = "paths"
+    if node_id:
+        if "-" in node_id:
+            path_type = "protpaths"
+        else:
+            path_type = "paths"
 
-    path_dn = "topology/pod-{0}/{1}-{2}/pathep-[{3}]".format(pod_id, path_type, node_id, path_ep)
+        path_dn = "topology/pod-{0}/{1}-{2}/pathep-[{3}]".format(pod_id, path_type, node_id, path_ep)
+
+    child_configs = []
+    child_classes = [
+      "bgpRsPeerPfxPol",
+      "bgpAsP",
+      "bgpLocalAsnP"
+    ]
+
+    if remote_asn:
+        child_configs.append(
+            dict(
+                bgpAsP=dict(
+                    attributes=dict(asn=remote_asn),
+                ),
+            )
+        )
+
+    if route_control_profiles:
+        child_classes.append("bgpRsPeerToProfile")
+        for profile in route_control_profiles:
+            if profile.get("l3out"):
+                route_control_profile_dn = "uni/tn-{0}/out-{1}/prof-{2}".format(
+                    profile.get("tenant"),
+                    profile.get("l3out"),
+                    profile.get("profile"),
+                )
+            else:
+                route_control_profile_dn = "uni/tn-{0}/prof-{1}".format(profile.get("tenant"), profile.get("profile"))
+            child_configs.append(
+                dict(
+                    bgpRsPeerToProfile=dict(
+                        attributes=dict(
+                            direction=profile.get("direction"),
+                            tDn=route_control_profile_dn,
+                        )
+                    )
+                )
+            )
+
+    subclass_3 = None
+    subclass_4 = None
+    subclass_5 = None
+
+    bgp_peer_profile_dict = None
+
+    if peer_ip or state == "query":
+        bgp_peer_profile_dict = dict(
+            aci_class="bgpPeerP",
+            aci_rn="peerP-[{0}]".format(peer_ip),
+            module_object=peer_ip,
+            target_filter={"addr": peer_ip},
+        )
+
+    if interface_profile is None:
+        subclass_3 = bgp_peer_profile_dict
+    else:
+        subclass_3 = dict(
+            aci_class="l3extLIfP",
+            aci_rn="lifp-{0}".format(interface_profile),
+            module_object=interface_profile,
+            target_filter={"name": interface_profile},
+        )
+        subclass_4 = dict(
+            aci_class="l3extRsPathL3OutAtt",
+            aci_rn="rspathL3OutAtt-[{0}]".format(path_dn),
+            module_object=path_dn,
+            target_filter={"tDn": path_dn},
+        )
+        subclass_5 = bgp_peer_profile_dict
 
     aci.construct_url(
         root_class=dict(
@@ -407,17 +568,10 @@ def main():
             module_object=node_profile,
             target_filter={"name": node_profile},
         ),
-        subclass_3=dict(
-            aci_class="l3extLIfP",
-            aci_rn="lifp-{0}".format(interface_profile),
-            module_object=interface_profile,
-            target_filter={"name": interface_profile},
-        ),
-        subclass_4=dict(
-            aci_class="l3extRsPathL3OutAtt", aci_rn="/rspathL3OutAtt-[{0}]".format(path_dn), module_object=path_dn, target_filter={"tDn": path_dn}
-        ),
-        subclass_5=dict(aci_class="bgpPeerP", aci_rn="/peerP-[{0}]".format(peer_ip), module_object=peer_ip, target_filter={"addr": peer_ip}),
-        child_classes=["bgpRsPeerPfxPol", "bgpAsP", "bgpLocalAsnP"],
+        subclass_3=subclass_3,
+        subclass_4=subclass_4,
+        subclass_5=subclass_5,
+        child_classes=child_classes,
     )
 
     aci.get_existing()
@@ -445,13 +599,7 @@ def main():
                 adminSt=admin_state,
                 allowedSelfAsCnt=allow_self_as_count,
             ),
-            child_configs=[
-                dict(
-                    bgpAsP=dict(
-                        attributes=dict(asn=remote_asn),
-                    ),
-                ),
-            ],
+            child_configs=child_configs,
         )
 
         aci.get_diff(aci_class="bgpPeerP")
