b0a14e9717726c4f53daf54dca22ac4071de19f0
timcragg <85495462+timcragg@users.noreply.github.com>
2023-04-18 14:23:19 +0100

    Update plugins/modules/aci_route_control_context.py
    Co-authored-by: Akini Ross <akinross@cisco.com>

--- plugins/modules/aci_route_control_context.py
+++ plugins/modules/aci_route_control_context.py
@@ -34,7 +34,7 @@ options:
     aliases: [ profile_name, route_control_profile ]
   context:
     description:
-    - Name of the Route Control Context
+    - Name of the Route Control Context.
     type: str
     aliases: [ name, route_control_context ]
   description:
