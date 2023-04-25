093caca58c16e0905ea93d687a6e28bb6fd7bde5
akinross <akinross@cisco.com>
2023-04-25 15:44:17 +0200

    [ignore] move destination_epg_spec to aci module utils for re-use by other modules

--- plugins/module_utils/aci.py
+++ plugins/module_utils/aci.py
@@ -252,6 +252,49 @@ def route_control_profile_spec():
     )
 
 
+def destination_epg_spec():
+    return dict(
+        tenant=dict(type="str", required=True, aliases=["tenant_name"]),
+        ap=dict(type="str", required=True, aliases=["ap_name", "app_profile", "app_profile_name"]),
+        epg=dict(type="str", required=True, aliases=["epg_name"]),
+        source_ip=dict(type="str", required=True),
+        destination_ip=dict(type="str", required=True),
+        span_version=dict(type="str", choices=["version_1", "version_2"]),
+        version_enforced=dict(type="bool"),
+        flow_id=dict(type="int"),
+        ttl=dict(type="int"),
+        mtu=dict(type="int"),
+        dscp=dict(
+            type="str",
+            choices=[
+                "CS0",
+                "CS1",
+                "CS2",
+                "CS3",
+                "CS4",
+                "CS5",
+                "CS6",
+                "CS7",
+                "EF",
+                "VA",
+                "AF11",
+                "AF12",
+                "AF13",
+                "AF21",
+                "AF22",
+                "AF23",
+                "AF31",
+                "AF32",
+                "AF33",
+                "AF41",
+                "AF42",
+                "AF43",
+                "unspecified",
+            ],
+        ),
+    )
+
+
 class ACIModule(object):
     def __init__(self, module):
         self.module = module
