9318c68134aed9ca5bd3090eef04ede8bbe6a87a
tcragg@cisco.com <tcragg@cisco.com>
2023-04-26 14:28:56 +0100

    fix: Sanity fix

--- plugins/modules/aci_export_encryption.py
+++ plugins/modules/aci_export_encryption.py
@@ -186,6 +186,7 @@ url:
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.cisco.aci.plugins.module_utils.aci import ACIModule, aci_argument_spec, aci_annotation_spec
 
+
 def main():
     argument_spec = aci_argument_spec()
     argument_spec.update(aci_annotation_spec())
