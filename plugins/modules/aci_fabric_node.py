e4617e0dc23e5c44912b146ee32d6fca75f1f180
Gaspard Micol <gmicol@cisco.com>
2023-05-14 14:05:30 -0400

    [minor_change] Add contact details and modify Pool_Id for aci_fabric_node

--- plugins/modules/aci_fabric_node.py
+++ plugins/modules/aci_fabric_node.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2017, Bruno Calogero <brunocalogero@hotmail.com>
+# Copyright: (c) 2023, Gaspard Micol <gmicol@cisco.com>
 # GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
