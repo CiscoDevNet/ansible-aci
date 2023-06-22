8a528372379e44c8c353b79075574e8e02919eda
Gaspard Micol <129436116+gmicol@users.noreply.github.com>
2023-06-22 11:12:54 -0400

    [ignore] Modify the documentation to suggest adding a pause task after creating a Snapshot for aci_config_rollback and aci_config_snapshot (#450)

--- plugins/modules/aci_config_rollback.py
+++ plugins/modules/aci_config_rollback.py
@@ -72,6 +72,9 @@ extends_documentation_fragment:
 - cisco.aci.aci
 - cisco.aci.annotation
 
+notes:
+- It is strongly recommended to add a pause task after creating a Snapshot.
+- Wait for the Snapshot to be finished before querying, comparing Snapshots or processing to Rollbacks.
 seealso:
 - module: cisco.aci.aci_config_snapshot
 - name: APIC Management Information Model reference
@@ -79,6 +82,7 @@ seealso:
   link: https://developer.cisco.com/docs/apic-mim-ref/
 author:
 - Jacob McGill (@jmcgill298)
+- Gaspard Micol (@gmicol)
 """
 
 EXAMPLES = r"""
@@ -91,6 +95,10 @@ EXAMPLES = r"""
     export_policy: config_backup
     state: present
   delegate_to: localhost
+
+- name: Wait for snapshot to be finished before querying
+  pause:
+    seconds: 10
 
 - name: Query Existing Snapshots
   cisco.aci.aci_config_snapshot:
