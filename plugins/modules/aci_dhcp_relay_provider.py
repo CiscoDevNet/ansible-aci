3a091d670be28419e884fb6d645668d6bec9df3c
akinross <akinross@cisco.com>
2023-08-29 16:21:19 +0200

    [minor_change] add support for global infra dhcp relay policy configuration in aci_dhcp_relay_provider

--- plugins/modules/aci_dhcp_relay_provider.py
+++ plugins/modules/aci_dhcp_relay_provider.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright: (c) 2022, Tim Cragg (@timcragg)
+# Copyright: (c) 2023, Akini Ross (@akinross)
 # GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
@@ -20,6 +21,7 @@ options:
   tenant:
     description:
     - Name of the tenant the relay_policy is in.
+    - When tenant is not provided the module will be applied to the global (infra) policy.
     type: str
   relay_policy:
     description:
@@ -29,14 +31,14 @@ options:
   provider_tenant:
     description:
     - Name of the tenant the epg or external_epg is in
-    - Only required if the epg or external_epg is in a different tenant than the relay_policy
+    - Required when epg or external_epg is in a different tenant than the relay_policy
+    - Required when global (infra) relay_policy is configured with epg or external_epg types
     type: str
   epg_type:
     description:
     - Type of EPG the DHCP server is in
     type: str
     choices: [ epg, l2_external, l3_external, dn ]
-    required: true
   anp:
     description:
     - Application Profile the EPG is in.
@@ -93,6 +95,7 @@ seealso:
   link: https://developer.cisco.com/docs/apic-mim-ref/
 author:
 - Tim Cragg (@timcragg)
+- Akini Ross (@akinross)
 """
 
 EXAMPLES = r"""
@@ -103,6 +106,20 @@ EXAMPLES = r"""
     password: SomeSecretPassword
     tenant: Auto-Demo
     relay_policy: my_dhcp_relay
+    epg_type: epg
+    anp: my_anp
+    epg: my_app_epg
+    dhcp_server_addr: 10.20.30.40
+    state: present
+  delegate_to: localhost
+
+- name: Add a new Global (infra) DHCP relay App EPG provider
+  cisco.aci.aci_dhcp_relay_provider:
+    host: apic
+    username: admin
+    password: SomeSecretPassword
+    relay_policy: my_dhcp_relay
+    provider_tenant: Auto-Demo
     epg_type: epg
     anp: my_anp
     epg: my_app_epg
@@ -137,6 +154,19 @@ EXAMPLES = r"""
     state: absent
   delegate_to: localhost
 
+- name: Remove a Global (infra) DHCP relay provider
+  cisco.aci.aci_dhcp_relay_provider:
+    host: apic
+    username: admin
+    password: SomeSecretPassword
+    relay_policy: my_dhcp_relay
+    provider_tenant: Auto-Demo
+    epg_type: epg
+    anp: my_anp
+    epg: my_app_epg
+    state: absent
+  delegate_to: localhost
+
 - name: Query a DHCP relay provider
   cisco.aci.aci_dhcp_relay_provider:
     host: apic
@@ -147,6 +177,39 @@ EXAMPLES = r"""
     epg_type: epg
     anp: my_anp
     epg: my_app_epg
+    state: query
+  delegate_to: localhost
+  register: query_result
+
+- name: Query a Global (infra) DHCP relay provider
+  cisco.aci.aci_dhcp_relay_provider:
+    host: apic
+    username: admin
+    password: SomeSecretPassword
+    relay_policy: my_dhcp_relay
+    provider_tenant: Auto-Demo
+    epg_type: epg
+    anp: my_anp
+    epg: my_app_epg
+    state: query
+  delegate_to: localhost
+  register: query_result
+
+- name: Query all DHCP relay providers in a specific tenant
+  cisco.aci.aci_dhcp_relay_provider:
+    host: apic
+    username: admin
+    password: SomeSecretPassword
+    tenant: Auto-Demo
+    state: query
+  delegate_to: localhost
+  register: query_result
+
+- name: Query all Global (infra) DHCP relay providers
+  cisco.aci.aci_dhcp_relay_provider:
+    host: apic
+    username: admin
+    password: SomeSecretPassword
     state: query
   delegate_to: localhost
   register: query_result
@@ -266,7 +329,7 @@ def main():
     argument_spec.update(aci_annotation_spec())
     argument_spec.update(
         relay_policy=dict(type="str", aliases=["relay_policy_name"]),
-        epg_type=dict(type="str", required=True, choices=["epg", "l2_external", "l3_external", "dn"]),
+        epg_type=dict(type="str", choices=["epg", "l2_external", "l3_external", "dn"]),
         anp=dict(type="str"),
         epg=dict(type="str", aliases=["app_epg"]),
         l2out_name=dict(type="str"),
@@ -283,8 +346,8 @@ def main():
         argument_spec=argument_spec,
         supports_check_mode=True,
         required_if=[
-            ["state", "absent", ["relay_policy", "tenant"]],
-            ["state", "present", ["relay_policy", "tenant"]],
+            ["state", "absent", ["relay_policy", "epg_type"]],
+            ["state", "present", ["relay_policy", "epg_type"]],
             ["epg_type", "epg", ["anp", "epg"]],
             ["epg_type", "l2_external", ["l2out_name", "external_epg"]],
             ["epg_type", "l3_external", ["l3out_name", "external_epg"]],
@@ -322,6 +385,9 @@ def main():
     if provider_tenant is None:
         provider_tenant = tenant
 
+    if epg_type is not None and epg_type != "dn" and provider_tenant is None:
+        module.fail_json(msg="provider_tenant is required when epg_type is {0}".format(epg_type))
+
     if epg_type == "epg":
         tdn = "uni/tn-{0}/ap-{1}/epg-{2}".format(provider_tenant, anp, epg)
     elif epg_type == "l2_external":
@@ -330,28 +396,46 @@ def main():
         tdn = "uni/tn-{0}/out-{1}/instP-{2}".format(provider_tenant, l3out_name, external_epg)
     elif epg_type == "dn":
         tdn = dn
+    else:
+        tdn = None
 
-    aci = ACIModule(module)
-    aci.construct_url(
-        root_class=dict(
-            aci_class="fvTenant",
-            aci_rn="tn-{0}".format(tenant),
-            module_object=tenant,
-            target_filter={"name": tenant},
-        ),
-        subclass_1=dict(
+    if tenant is None:
+        root_class = dict(
             aci_class="dhcpRelayP",
-            aci_rn="relayp-{0}".format(relay_policy),
+            aci_rn="infra/relayp-{0}".format(relay_policy),
             module_object=relay_policy,
             target_filter={"name": relay_policy},
-        ),
-        subclass_2=dict(
+        )
+        subclass_1 = dict(
             aci_class="dhcpRsProv",
             aci_rn="rsprov-[{0}]".format(tdn),
             module_object=tdn,
             target_filter={"tDn": tdn},
-        ),
-    )
+        )
+        subclass_2 = None
+    else:
+        root_class = dict(
+            aci_class="fvTenant",
+            aci_rn="tn-{0}".format(tenant),
+            module_object=tenant,
+            target_filter={"name": tenant},
+        )
+        subclass_1 = dict(
+            aci_class="dhcpRelayP",
+            aci_rn="relayp-{0}".format(relay_policy),
+            module_object=relay_policy,
+            target_filter={"name": relay_policy},
+        )
+        subclass_2 = dict(
+            aci_class="dhcpRsProv",
+            aci_rn="rsprov-[{0}]".format(tdn),
+            module_object=tdn,
+            target_filter={"tDn": tdn},
+        )
+
+    aci = ACIModule(module)
+
+    aci.construct_url(root_class=root_class, subclass_1=subclass_1, subclass_2=subclass_2)
 
     aci.get_existing()
 
