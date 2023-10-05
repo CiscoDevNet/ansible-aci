d88850d360ff717c41982aa1b6de12cb548f16ed
Gaspard Micol <gmicol@cisco.com>
2023-10-05 10:21:28 -0400

    [ignore] Add Tim Cragg as author in the modules he worked on.

--- plugins/modules/aci_match_route_destination.py
+++ plugins/modules/aci_match_route_destination.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright: (c) 2023, Gaspard Micol (@gmicol) <gmicol@cisco.com>
+# Copyright: (c) 2023, Tim Cragg (@timcragg) <tcragg@cisco.com>
 # GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
@@ -78,6 +79,7 @@ seealso:
   link: https://developer.cisco.com/docs/apic-mim-ref/
 author:
 - Gaspard Micol (@gmicol)
+- Tim Cragg (@timcragg)
 """
 
 EXAMPLES = r"""
