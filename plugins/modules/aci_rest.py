8c5085f07cec9da8fa4029e7a86bf2a0f17e62a6
samitab <samitab@cisco.com>
2023-10-25 07:26:29 +1000

    [ignore] Added all unsupported annotation classes.

--- plugins/modules/aci_rest.py
+++ plugins/modules/aci_rest.py
@@ -290,8 +290,9 @@ except Exception:
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.cisco.aci.plugins.module_utils.aci import ACIModule, aci_argument_spec, aci_annotation_spec
 from ansible.module_utils._text import to_text
-
-ANNOTATION_UNSUPPORTED = ["tagTag"]
+from ansible_collections.cisco.aci.plugins.module_utils.annotation_unsupported import (
+    ANNOTATION_UNSUPPORTED,
+)
 
 
 def update_qsl(url, params):
