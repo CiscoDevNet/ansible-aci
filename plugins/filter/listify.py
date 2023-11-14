8cf8a722f68e6647715d629fcc754cb8107b507b
Shreyas <ssrish@cisco.com>
2023-11-14 12:15:58 -0500

    [ignore] Added a new tenant to the data being fed into the 'listify plugin' test case for additional checks

--- plugins/filter/listify.py
+++ plugins/filter/listify.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 # Copyright: (c) 2017, Ramses Smeyers <rsmeyers@cisco.com>
 # Copyright: (c) 2023, Shreyas Srish <ssrish@cisco.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
