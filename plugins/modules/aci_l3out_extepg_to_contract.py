181bc2a1243c5b20456fa7dcde4ba10eb2506dd6
akinross <akinross@cisco.com>
2023-12-06 12:08:43 +0100

    [minor_change] add support for taboo contract, contract interface and intra_epg contract in aci_l3out_extepg_to_contract

--- plugins/modules/aci_l3out_extepg_to_contract.py
+++ plugins/modules/aci_l3out_extepg_to_contract.py
@@ -3,6 +3,7 @@
 
 # Copyright: (c) 2020, Sudhakar Shet Kudtarkar (@kudtarkar1)
 # Copyright: (c) 2020, Shreyas Srish <ssrish@cisco.com>
+# Copyright: (c) 2023, Akini Ross (@akinross) <akinross@cisco.com>
 # GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
@@ -14,7 +15,7 @@ ANSIBLE_METADATA = {"metadata_version": "1.1", "status": ["preview"], "supported
 DOCUMENTATION = r"""
 ---
 module: aci_l3out_extepg_to_contract
-short_description: Bind Contracts to External End Point Groups (EPGs)
+short_description: Bind Contracts to External End Point Groups (EPGs) (fv:RsCons, fv:RsProv, fv:RsProtBy, fv:RsConsIf, fv:RsIntraEpg)
 description:
 - Bind Contracts to External End Point Groups (EPGs) on ACI fabrics.
 options:
@@ -26,32 +27,43 @@ options:
     description:
     - Name of the l3out.
     type: str
-    aliases: ['l3out_name']
+    aliases: [ l3out_name ]
   extepg:
     description:
     - Name of the external end point group.
     type: str
-    aliases: ['extepg_name', 'external_epg']
+    aliases: [ extepg_name, external_epg]
   contract:
     description:
-    - Name of the contract.
+    - The name of the contract or contract interface.
     type: str
+    aliases: [ contract_name, contract_interface ]
   contract_type:
     description:
-    - The type of contract.
+    - Determines the type of the Contract.
     type: str
     required: true
-    choices: ['consumer', 'provider']
+    choices: [ consumer, provider, taboo, interface, intra_epg ]
   priority:
     description:
-    - This has four levels of priority.
+    - QoS class.
+    - The APIC defaults to C(unspecified) when unset during creation.
     type: str
-    choices: ['level1', 'level2', 'level3', 'unspecified']
+    choices: [ level1, level2, level3, level4, level5, level6, unspecified ]
   provider_match:
     description:
-    - This is configurable for provided contracts.
+    - The matching algorithm for Provided Contracts.
+    - The APIC defaults to C(at_least_one) when unset during creation.
     type: str
-    choices: ['all', 'at_least_one', 'at_most_one', 'none']
+    choices: [ all, at_least_one, at_most_one, none ]
+  contract_label:
+    description:
+    - Contract label to match.
+    type: str
+  subject_label:
+    description:
+    - Subject label to match.
+    type: str
   state:
     description:
     - Use C(present) or C(absent) for adding or removing.
@@ -64,15 +76,16 @@ extends_documentation_fragment:
 - cisco.aci.annotation
 
 notes:
-- The C(tenant), C(l3out) and C(extepg) must exist before using this module in your playbook.
-  The M(cisco.aci.aci_tenant), M(cisco.aci.aci_l3out) and M(cisco.aci.aci_l3out_extepg) modules can be used for this.
+- The C(tenant), C(l3out), C(extepg), and C(Contract) must exist before using this module in your playbook.
+  The M(cisco.aci.aci_tenant), M(cisco.aci.aci_l3out), M(cisco.aci.aci_l3out_extepg), and M(cisco.aci.aci_contract) modules can be used for this.
 seealso:
 - name: APIC Management Information Model reference
-  description: More information about the internal APIC class B(fvtenant), B(l3extInstP) and B(l3extOut).
+  description: More information about the internal APIC class B(fv:RsCons), B(fv:RsProv), B(fv:RsProtBy, B(fv:RsConsIf, and B(fv:RsIntraEpg).
   link: https://developer.cisco.com/docs/apic-mim-ref/
 author:
 - Sudhakar Shet Kudtarkar (@kudtarkar1)
 - Shreyas Srish (@shrsr)
+- Akini Ross (@akinross)
 """
 
 EXAMPLES = r"""
@@ -234,38 +247,23 @@ RETURN = r"""
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.cisco.aci.plugins.module_utils.aci import ACIModule, aci_argument_spec, aci_annotation_spec
-
-ACI_CLASS_MAPPING = dict(
-    consumer={
-        "class": "fvRsCons",
-        "rn": "rscons-",
-    },
-    provider={
-        "class": "fvRsProv",
-        "rn": "rsprov-",
-    },
-)
-
-PROVIDER_MATCH_MAPPING = dict(
-    all="All",
-    at_least_one="AtleastOne",
-    at_most_one="tmostOne",
-    none="None",
-)
+from ansible_collections.cisco.aci.plugins.module_utils.constants import ACI_CLASS_MAPPING, CONTRACT_LABEL_MAPPING, PROVIDER_MATCH_MAPPING, SUBJ_LABEL_MAPPING
 
 
 def main():
     argument_spec = aci_argument_spec()
     argument_spec.update(aci_annotation_spec())
     argument_spec.update(
-        contract_type=dict(type="str", required=True, choices=["consumer", "provider"]),
+        contract_type=dict(type="str", required=True, choices=["consumer", "provider", "taboo", "interface", "intra_epg"]),
         l3out=dict(type="str", aliases=["l3out_name"]),
-        contract=dict(type="str"),
-        priority=dict(type="str", choices=["level1", "level2", "level3", "unspecified"]),
+        contract=dict(type="str", aliases=["contract_name", "contract_interface"]),  # Not required for querying all objects
+        priority=dict(type="str", choices=["level1", "level2", "level3", "level4", "level5", "level6", "unspecified"]),
         provider_match=dict(type="str", choices=["all", "at_least_one", "at_most_one", "none"]),
         state=dict(type="str", default="present", choices=["absent", "present", "query"]),
         tenant=dict(type="str"),
         extepg=dict(type="str", aliases=["extepg_name", "external_epg"]),
+        contract_label=dict(type="str"),
+        subject_label=dict(type="str"),
     )
     module = AnsibleModule(
         argument_spec=argument_spec,
@@ -286,12 +284,24 @@ def main():
         provider_match = PROVIDER_MATCH_MAPPING.get(provider_match)
     state = module.params.get("state")
     tenant = module.params.get("tenant")
+    contract_label = module.params.get("contract_label")
+    subject_label = module.params.get("subject_label")
 
-    aci_class = ACI_CLASS_MAPPING.get(contract_type)["class"]
-    aci_rn = ACI_CLASS_MAPPING.get(contract_type)["rn"]
+    aci_class = ACI_CLASS_MAPPING[contract_type]["class"]
+    aci_rn = ACI_CLASS_MAPPING[contract_type]["rn"]
+    aci_name = ACI_CLASS_MAPPING[contract_type]["name"]
+    child_classes = []
 
-    if contract_type == "consumer" and provider_match is not None:
+    if contract_type != "provider" and provider_match is not None:
         module.fail_json(msg="the 'provider_match' is only configurable for Provided Contracts")
+
+    if contract_type in ["taboo", "interface", "intra_epg"] and (contract_label is not None or subject_label is not None):
+        module.fail_json(msg="the 'contract_label' and 'subject_label' are not configurable for {0} contracts".format(contract_type))
+
+    if contract_type not in ["taboo", "interface", "intra_epg"]:
+        contract_label_class = CONTRACT_LABEL_MAPPING.get(contract_type)
+        subject_label_class = SUBJ_LABEL_MAPPING.get(contract_type)
+        child_classes = [subject_label_class, contract_label_class]
 
     aci = ACIModule(module)
     aci.construct_url(
@@ -317,20 +327,23 @@ def main():
             aci_class=aci_class,
             aci_rn="{0}{1}".format(aci_rn, contract),
             module_object=contract,
-            target_filter={"tnVzBrCPName": contract},
+            target_filter={aci_name: contract},
         ),
+        child_classes=child_classes,
     )
 
     aci.get_existing()
 
     if state == "present":
+        child_configs = []
+        if contract_label is not None:
+            child_configs.append({contract_label_class: {"attributes": {"name": contract_label}}})
+        if subject_label is not None:
+            child_configs.append({subject_label_class: {"attributes": {"name": subject_label}}})
         aci.payload(
             aci_class=aci_class,
-            class_config=dict(
-                matchT=provider_match,
-                prio=priority,
-                tnVzBrCPName=contract,
-            ),
+            class_config={"matchT": provider_match, "prio": priority, aci_name: contract},
+            child_configs=child_configs,
         )
 
         aci.get_diff(aci_class=aci_class)
