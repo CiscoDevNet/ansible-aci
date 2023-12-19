a1e2e9502ba96e720c544aa85d5fd8ceb0e97949
Gaspard Micol <gmicol@cisco.com>
2023-12-19 17:30:17 -0500

    [ignore] Fix Galaxy Importer error for all extra bgp modules.

--- plugins/modules/aci_bgp_peer_prefix_policy.py
+++ plugins/modules/aci_bgp_peer_prefix_policy.py
@@ -238,7 +238,6 @@ url:
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.cisco.aci.plugins.module_utils.aci import ACIModule, aci_argument_spec, aci_annotation_spec, aci_owner_spec
-from ansible_collections.cisco.aci.plugins.module_utils.constants import MATCH_GRACEFUL_RESTART_CONTROLS_MAPPING
 
 
 def main():
