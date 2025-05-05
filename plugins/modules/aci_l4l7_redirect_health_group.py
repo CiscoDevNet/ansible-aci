268646f0afc1912fc707f7cafb16d63b2285fcc2
Shreyas <ssrish@cisco.com>
2025-05-05 12:34:02 -0400

    [ignore] addition of aliases to policy in aci_l4l7_policy_based_redirect

--- plugins/modules/aci_l4l7_redirect_health_group.py
+++ plugins/modules/aci_l4l7_redirect_health_group.py
@@ -42,6 +42,7 @@ notes:
 - The I(tenant) must exist before using this module in your playbook.
   The M(cisco.aci.aci_tenant) module can be used for this.
 seealso:
+module: aci_tenant
 - name: APIC Management Information Model reference
   description: More information about the internal APIC class B(vns:RedirectHealthGroup)
   link: https://developer.cisco.com/docs/apic-mim-ref/
